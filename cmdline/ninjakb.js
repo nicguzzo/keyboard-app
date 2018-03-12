34601c5a4ca0c45b09aa82d44a2674db8848bbb6
Marcelo Alaniz <alanizmarcelo@gmail.com>
2018-03-12 00:09:48 -0300

    cmdline with js

--- cmdline/ninjakb.js
+++ cmdline/ninjakb.js
@@ -1,42 +1,44 @@
 
-var fs = require('fs');
-var conf = JSON.parse(fs.readFileSync('../conf/conf.json', 'utf8'));
-var scancodes = JSON.parse(fs.readFileSync('../frontend/public/scancodes.json', 'utf8'));
-var SerialPort = require('serialport');
-var port = new SerialPort('/dev/ttyACM0', {
+const fs = require('fs');
+const conf = JSON.parse(fs.readFileSync('../conf/conf.json', 'utf8'));
+const scancodes = JSON.parse(fs.readFileSync('../frontend/public/scancodes.json', 'utf8'));
+const SerialPort = require('serialport');
+const port = new SerialPort('/dev/ttyACM0', {
   baudRate: 115200
 });
-function sendKeys(layer,side){
-  const { layers } = conf;
-  layers[layer][side].map( (key, i) => {
-    const n =`0${i}`.slice(-2);
-    //console.log(this.scancodes);
-    let t='c';
-    let k=null;
-    if( scancodes.modifiers.indexOf(key)>=0){
-      t='m';
-      k=key;
-    }else{
-      k=scancodes.codes[key];
-    }
-    if(k){
-      const cmd = `${side.slice(0,1)}${layer}${n}${t}${k}`;
-      //this.socket.send(cmd);
-      
-      port.write(cmd,function(){
-        port.flush();
-      });
-      //setTimeout(function() {
-      //  console.log('...');
-      //}, 500);
-      console.log(cmd);
-    }
-    return true;
-  });
-  
+
+async function processKey(key, i, side, layer){
+  const n =`0${i}`.slice(-2);
+  let t='c';
+  let k=null;
+  if( scancodes.modifiers.indexOf(key)>=0){
+    t='m';
+    k=key;
+  }else{
+    k=scancodes.codes[key];
+  }
+  if(k){
+    const cmd = `${side.slice(0,1)}${layer}${n}${t}${k}`;
+    //this.socket.send(cmd);
+
+    port.write(cmd,function(){
+      port.flush();
+    });
+    console.log(cmd);
+  }
 }
+async function sendKeys(layer,side){
+  const { layers } = conf;
+  const keys = layers[layer][side];
+  console.log(keys);
+  for (let i=0; i<keys.length; i++){
+    await processKey(keys[i], i, side, layer);
+  }
+}
+
 port.on("open",function(){
   sendKeys(1,'left');
-  sendKeys(1,'right');
-  port.write("save");
+  //sendKeys(1,'right');
+  //port.write("save");
+  console.log('All keys sended');
 });
